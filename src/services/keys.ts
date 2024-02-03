b0c53ed71d6ed72b7c3604314eda5d7eb25da0f5
Angrypig123 <shguddnr3@naver.com>
2024-02-04 00:40:08 +0900

    test area or redis client practice

---
+++ src/services/keys.ts
@@ -0,0 +1,2 @@
+export const pageCacheKey = (id: String) => `pagecache#${id}`;
+export const userKey = (userId: String) => `users#${userId}`;
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,20 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { userKey } from '$services/keys';
+import { client } from '$services/redis';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
 
-export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+};
+
+export const getUserById = async (id: string) => { };
+
+export const createUser = async (attrs: CreateUserAttrs) => {
+    const id = genId();
+    await client.hSet(userKey(id), {
+        username: attrs.username,
+        password: attrs.password
+    });
+    return id;
+};
