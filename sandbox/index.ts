a1337c1f3bce9d311dec24d4e131c1573467d24f
AngryPig123 <86225268+AngryPig123@users.noreply.github.com>
2024-02-04 00:40:34 +0900

    test area or redis client practice (#1)

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,25 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
+import { genId } from '../src/services/utils';
 
-const run = async () => {};
+const run = async () => {
+    await client.del('car');
+    await client.hSet('car', {
+        color: 'red',
+        year: 1950
+    });
+    //  HSET car color red year 1950
+    const car = await client.hGetAll('car');
+    if (Object.keys(car).length === 0) {
+        console.log('Car not found, respond with 404');
+        return;
+    }
+    console.log(car);
+
+    const keyList = await client.keys('*');
+    console.log(keyList);
+  
+    
+};
+
 run();
